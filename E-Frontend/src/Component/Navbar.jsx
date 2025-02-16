34ed6c95a0c1cfc1ded55eb80b4f72f2a214b09a
HaiderAli777 <ha7325897@gmail.com>
2025-02-16 10:46:58 -0800

    Making the Navbar and make it resposive

--- E-Frontend/src/Component/Navbar.jsx
+++ E-Frontend/src/Component/Navbar.jsx
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import imgage1 from "../assets/logo.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 export default function Navbar() {
+  const [pop, setpop] = useState(false);
+
+  const ShowtheMenu = () => {
+    setpop(true);
+  };
+
+  const ClosetheMenu = () => {
+    setpop(false);
+  };
+
   return (
-    <div>
-      <div className="lg:justify-between flex flex-row bg-gray-100  py-4 px-14  items-center">
-        <div className="p-1">
-          <img src={imgage1} className="max-w-14"></img>
+    <>
+      <div>
+        <div className="justify-between flex flex-row bg-gray-100  py-4 px-14  items-center">
+          <div className="p-1">
+            <img src={imgage1} className="max-w-14"></img>
+          </div>
+          <div className="hidden md:flex space-x-6 font-bold text-blue-800 ">
+            <h1>Home</h1>
+            <h1>About</h1>
+            <h1>Contact Me</h1>
+          </div>
+          <div className="hidden md:flex space-x-5 flex-row font-bold text-blue-800">
+            <h1>Sign In</h1>
+            <h1>Sign Up</h1>
+          </div>
+          <div className="md:hidden">
+            <button onClick={ShowtheMenu}>
+              {pop || <FontAwesomeIcon icon={faBars} />}
+            </button>
+            <button onClick={ClosetheMenu}>
+              {pop && <FontAwesomeIcon icon={faXmark} />}
+            </button>
+          </div>
         </div>
-        <div className="hidden lg:flex space-x-6 font-bold text-blue-800 ">
+      </div>
+      {pop && (
+        <div
+          className={`bg-blue-300 flex-col py-5 space-y-1.5 text-center fixed right-0 left-0 md:hidden`}
+        >
+          <h1 className="">Sign In</h1>
+          <h1>Sign Up</h1>
           <h1>Home</h1>
           <h1>About</h1>
           <h1>Contact Me</h1>
         </div>
-        <div className="hidden lg:flex space-x-5 flex-row font-bold text-blue-800">
-          <h1>Sign In</h1>
-          <h1>Sign Up</h1>
-        </div>
-        <dv></dv>
-      </div>
-      <div className="bg-blue-300 flex-col py-5 space-y-1.5 text-center fixed right-0 left-0">
-        <h1 className="">Sign In</h1>
-        <h1>Sign Up</h1>
-        <h1>Home</h1>
-        <h1>About</h1>
-        <h1>Contact Me</h1>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
